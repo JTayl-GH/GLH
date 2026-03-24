ec05895f463d708a2b4ee6e2e1527f3cf2d259a5
JTayl-GH <jtaylor291007@gmail.com>
2026-03-24 15:01:20 +0000

    darkmode added and adjust to match website.

--- project/public/js/darkmode.js
+++ project/public/js/darkmode.js
@@ -1,4 +1,4 @@
-const toggleBtn = document.getElementById("toggle-darkmode");
+const toggleBtn = document.getElementById("darkmode-toggle");
 
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
